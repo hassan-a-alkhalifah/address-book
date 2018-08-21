7a3bf18e35d1b6f86abd94dcf0c7f16f348edfc1
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-21 00:17:02 -0700

    add html to take user input, js scripting to store user input to display on a form submit event, style form

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// UI logic
+$(document).ready(function() {
+  $("#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("#new-first-name").val();
+    var inputtedLastName = $("#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("#new-first-name").val("");
+    $("#new-last-name").val("");
+  });
+});
